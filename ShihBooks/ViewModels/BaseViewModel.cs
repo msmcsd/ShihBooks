faf759f278cd80df24c4ff5f7da4ec51a4e95561
Jason Shih <jason_shih@hotmail.com>
2023-09-08 15:09:04 -0700

    Add BaseViewModel and more format on TemplateItem

---
+++ ShihBooks/ViewModels/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ShihBooks.ViewModels
+{
+    public partial class BaseViewModel : ObservableObject
+    {
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+        private bool _isBusy;
+
+        public bool IsNotBusy => !IsBusy;
+
+        public BaseViewModel()
+        {
+            
+        }
+    }
+}
