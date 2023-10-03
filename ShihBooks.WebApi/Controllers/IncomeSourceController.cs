ac53638150dfac40d97ed1856b9796035018c8d9
Jason Shih <jason_shih@hotmail.com>
2023-10-03 16:14:21 -0700

    Add IncomeSourceController

---
+++ ShihBooks.WebApi/Controllers/IncomeSourceController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShihBooks.WebApi.Models;
+
+namespace ShihBooks.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/incomesource")]
+    public class IncomeSourceController : ControllerBase
+    {
+        private readonly ILogger<IncomeSourceController> _logger;
+
+        public IncomeSourceController(ILogger<IncomeSourceController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<IncomeSource>> GetTypes(ApplicationDbContext db)
+        {
+            return await db.IncomeSources.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task AddType(ApplicationDbContext db, IncomeSource incomeSource)
+        {
+            var t = await db.IncomeSources.FirstOrDefaultAsync(e => e.Name.Equals(incomeSource.Name, StringComparison.InvariantCultureIgnoreCase));
+            if (t != null)
+            {
+                if (t.Name != incomeSource.Name)
+                {
+                    await UpdateType(db, incomeSource.Id, incomeSource.Name);
+                }
+                return;
+            }
+
+            db.IncomeSources.Add(incomeSource);
+            await db.SaveChangesAsync();
+        }
+
+        [HttpPut]
+        public async Task UpdateType(ApplicationDbContext db, int id, string newTypeName)
+        {
+            var ev = await db.IncomeSources.FindAsync(id);
+            if (ev != null)
+            {
+                ev.Name = newTypeName;
+                await db.SaveChangesAsync();
+            }
+        }
+
+        [HttpDelete]
+        public async Task DeleteType(ApplicationDbContext db, int id)
+        {
+            var e = await db.IncomeSources.FirstOrDefaultAsync(e => e.Id == id);
+            if (e != null)
+            {
+                return;
+            }
+
+            var ev = await db.IncomeSources.FindAsync(id);
+            if (ev != null)
+            {
+                db.IncomeSources.Remove(ev);
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+
+}
