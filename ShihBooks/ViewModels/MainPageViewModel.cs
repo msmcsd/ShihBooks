170edfac7a0f874aff5ab2992de75203f8a6221e
Jason Shih <jason_shih@hotmail.com>
2023-10-06 16:36:52 -0700

    Add Expense/Income buttons to main page

--- ShihBooks/ViewModels/MainPageViewModel.cs
+++ ShihBooks/ViewModels/MainPageViewModel.cs
@@ -7,11 +7,35 @@ namespace ShihBooks.ViewModels
     {
 
         [RelayCommand]
-        public async Task ShowCurrentMonthExpenses()
+        public async Task ShowCurrentMonthExpensesAsync()
         {
             if (IsBusy) return;
 
             await Shell.Current.GoToAsync($"{nameof(ExpensesPage)}?year={DateTime.Now.Year}&month={DateTime.Now.Month}");
+        }
+
+        [RelayCommand]
+        public async Task AddExpenseAsync()
+        {
+            if (IsBusy) return;
+
+            await Shell.Current.GoToAsync($"{nameof(ExpenseDetailsPage)}", true,
+                new Dictionary<string, object>()
+                {
+                    { "Expense", null }
+                });
+        }
+
+        [RelayCommand]
+        public async Task ShowCurrentMonthIncomesAsync()
+        {
+            if (IsBusy) return;
+        }
+
+        [RelayCommand]
+        public async Task AddIncomeAsync()
+        {
+            if (IsBusy) return;
         }
     }
 }
