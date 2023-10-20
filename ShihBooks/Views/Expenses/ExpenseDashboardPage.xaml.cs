209a61c6ec09d5d545ce0a9f593756cf1a2eb0c0
Jason Shih <jason_shih@hotmail.com>
2023-10-20 15:52:58 -0700

    Add Income and Expense dashboard pages

---
+++ ShihBooks/Views/Expenses/ExpenseDashboardPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace ShihBooks.Views.Expenses;
+
+public partial class ExpenseDashboardPage : ContentPage
+{
+	public ExpenseDashboardPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ShihBooks/Views/Incomes/IncomeDashboardPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace ShihBooks.Views.Incomes;
+
+public partial class IncomeDashboardPage : ContentPage
+{
+	public IncomeDashboardPage()
+	{
+		InitializeComponent();
+	}
+}
